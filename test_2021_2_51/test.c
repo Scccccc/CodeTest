efac19bee108de36b21350cdf09c11b6d32bce90
Sccccc <yishengaaaa@gmail.com>
2021-02-05 16:20:23 +0200

    .....

---
+++ test_2021_2_51/test.c
@@ -0,0 +1,103 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+#include <string.h>
+
+//int Fun(int n)
+//{
+//	if (n == 5)
+//		return 2;
+//	else
+//		return 2 * Fun(n + 1);
+//}
+//int main()
+//{
+//	int n = 2;
+//	int ret = Fun(n);
+//	printf("%d", ret);
+//	return 0;
+//}
+
+//void print(int n)
+//{
+//	if (n > 9)
+//	{
+//		print(n / 10);
+//	}
+//	printf("%d ", n % 10);
+//}
+//
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	print(n);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int n = 0;
+//	int i = 0;
+//	int sum = 1;
+//	scanf("%d", &n);
+//	for (i = 1; i <= n; i++)
+//	{
+//		sum *= i;
+//	}
+//	printf("%d", sum);
+//	return 0;
+//}
+
+//int Fun(int n)
+//{
+//	if (n <= 1)
+//		return 1;
+//	else
+//		return n * Fun(n - 1);
+//}
+//
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	int ret = Fun(n);
+//	printf("%d\n", ret);
+//	return 0;
+//}
+//
+//int My_strlen(char * str)
+//{
+//	int count = 0;
+//	while (*str != '\0')
+//	{
+//		count++;
+//		str++;
+//	}
+//	return count;
+//}
+//int main()
+//{
+//	char arr[] = {"asdsafsa"};
+//	int count = My_strlen(arr);
+//	printf("%d\n", count);
+//	return 0;
+//}
+
+//int MY_strlen(char* s)
+//{
+//	if (*s == '\0')
+//	{
+//		return 0;
+//	}
+//	else
+//		return 1 + MY_strlen(s + 1);
+//}
+//int main()
+//{
+//	char arr[] = { "asdsas" };
+//	int ret = MY_strlen(arr);
+//	printf("%d", ret);
+//	return 0;
+//}
+
