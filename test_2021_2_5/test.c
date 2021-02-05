0d0e7b320f936feb54cd07be8dc0e146e7d6d02e
Sccccc <yishengaaaa@gmail.com>
2021-02-05 09:05:36 +0200

    2.5

---
+++ test_2021_2_5/test.c
@@ -0,0 +1,79 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+//int main()
+//{
+//	int a = 0x11223344;
+//	int* p = &a;
+//	*p = 0;
+//	return 0;
+//}
+//
+//int* test()
+//{
+//	static int a = 10;
+//	return &a;
+//}
+//int main()
+//{
+//	int* p = test();
+//	printf("hehe\n");
+//	printf("%d", *p);
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	int arr[5] = { 0 };
+//	int* p = arr;
+//	int i = 0;
+//	for (i = 0; i < 5; i++)
+//	{
+//		*(p + i) = i;
+//	}
+//	for (i = 0; i < 5; i++)
+//	{
+//		printf("%d ", *(p + i));
+//	}
+//	return 0;
+//}
+
+//int my_strlen(char* str)
+//{
+//	char* start = str;
+//	while (*str != '\0')
+//	{
+//		str++;
+//	}
+//	return str - start;
+//}
+//int main()
+//{
+//	char arr[] = "abdfef";
+//	int len = my_strlen(arr);
+//	printf("%d", len);
+//	return 0;
+//}
+
+
+//int My_strlen(char* arr)
+//{
+//	int count = 0;
+//	while (*arr != '\0')
+//	{
+//		count++;
+//		arr++;
+//	}
+//	return count;
+//}
+//int main()
+//{
+//	char arr[] = "abcdef";
+//	int len = My_strlen(arr);
+//	printf("%d", len);
+//	return 0;
+//}
+
